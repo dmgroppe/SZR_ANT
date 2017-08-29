cb90082c50bedb2c4ac0630578aae885df151e78
David Groppe <dgroppe@cogsci.ucsd.edu>
2017-08-28 22:10:21 -0400

    post-ictals house cleaning

--- EU_CODE/szr_onset2df.py
+++ EU_CODE/szr_onset2df.py
@@ -29,15 +29,27 @@ htable=pd.read_html(html_fname)
 print('%d tables read' % len(htable))
 print('This code expects 4 tables.')
 
+# Purge "Total:" rows
+for tloop in range(len(htable)):
+    n_rows = htable[tloop].shape[0]
+    #if np.isnan(htable[tloop].iloc[n_rows - 1, 2]):
+    if htable[tloop].iloc[n_rows-1,0]=='total:':
+        htable[tloop]=htable[tloop].drop(htable[tloop].index[n_rows-1])
+
+# print(htable[0])
+# print(htable[2])
+# exit()
+
 # FIRST TABLE IS CLINICAL SZRS
 # Convert clinical onset/offsets to datetime
 n_clinical_szr=htable[0].shape[0]
-print('%d clinical szrs' % n_clinical_szr)
+print('************** %d clinical szrs' % n_clinical_szr)
 szr_onset_sec=list()
 szr_offset_sec=list()
 szr_onset_str=list()
 szr_offset_str=list()
 szr_type=list()
+
 
 #milestone=datetime(1970,1,1) # This is 5 hours (I think) after time=0 for python
 milestone=datetime(2000,1,1) # Arbirary date via which to convert times into seconds
@@ -48,67 +60,88 @@ for a in range(n_clinical_szr):
     
     print()
     # Convert onset to seconds since milestone
-    temp_lst=htable[0].iloc[a,2].split(' ')
-    temp_lst[1]=temp_lst[1].replace("'","20")
-    print('Onset: '+temp_lst[1]+' '+temp_lst[2])
-    hr_splt=temp_lst[2].split(':')
-    temp_dt=datetime.strptime(temp_lst[1],'%d.%m.%Y')
-    ttl_sec=(temp_dt-milestone).total_seconds()
-    ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
-    szr_onset_sec.append(ttl_sec)
-    szr_onset_str.append(temp_lst[1]+' '+temp_lst[2])
+    if not isinstance(htable[0].iloc[a, 2], str):
+        szr_onset_sec.append(np.nan)
+        szr_onset_str.append('NaN')
+    else:
+        temp_lst=htable[0].iloc[a,2].split(' ')
+        temp_lst[1]=temp_lst[1].replace("'","20")
+        print('Onset: '+temp_lst[1]+' '+temp_lst[2])
+        hr_splt=temp_lst[2].split(':')
+        temp_dt=datetime.strptime(temp_lst[1],'%d.%m.%Y')
+        ttl_sec=(temp_dt-milestone).total_seconds()
+        ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
+        szr_onset_sec.append(ttl_sec)
+        szr_onset_str.append(temp_lst[1]+' '+temp_lst[2])
     
     # Convert offset to seconds since milestone
-    temp_lst=htable[0].iloc[a,3].split(' ')
-    temp_lst[1]=temp_lst[1].replace("'","20")
-    print('Offset: '+temp_lst[1]+' '+temp_lst[2])
-    hr_splt=temp_lst[2].split(':')
-    temp_dt=datetime.strptime(temp_lst[1],'%d.%m.%Y')
-    ttl_sec=(temp_dt-milestone).total_seconds()
-    ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
-    szr_offset_sec.append(ttl_sec)
-    szr_offset_str.append(temp_lst[1]+' '+temp_lst[2])
+    if not isinstance(htable[0].iloc[a, 3], str):
+        szr_offset_sec.append(np.nan)
+        szr_offset_str.append('NaN')
+    else:
+        temp_lst=htable[0].iloc[a,3].split(' ')
+        temp_lst[1]=temp_lst[1].replace("'","20")
+        print('Offset: '+temp_lst[1]+' '+temp_lst[2])
+        hr_splt=temp_lst[2].split(':')
+        temp_dt=datetime.strptime(temp_lst[1],'%d.%m.%Y')
+        ttl_sec=(temp_dt-milestone).total_seconds()
+        ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
+        szr_offset_sec.append(ttl_sec)
+        szr_offset_str.append(temp_lst[1]+' '+temp_lst[2])
 
     df=szr_offset_sec[a]-szr_onset_sec[a]
     mnts=np.floor(df/60)
     scs=df-mnts*60
     print('Duration: %d minute(s) %f sec' % (mnts,scs))
     
-    
-# THIRD TABLE IS CLINICAL SZRS
+
+# THIRD TABLE IS SUBCLINICAL SZRS
 # Convert SUBclinical onset/offsets to datetime
-n_subclinical_szr=htable[2].shape[0]-1 # subtract 1 because last row is a total
-print('%d subclinical szrs' % n_subclinical_szr)
+#n_subclinical_szr=htable[2].shape[0]-1 # subtract 1 because last row is a total
+n_subclinical_szr=htable[2].shape[0] # subtract 1 because last row is a total
+print('************** %d subclinical szrs' % n_subclinical_szr)
 
 for a in range(n_subclinical_szr):
     szr_type.append('Subclinical')
         
     print()
     # Convert onset to seconds since milestone
-    temp_lst=htable[2].iloc[a,1].split(' ')
-    temp_lst[0]=temp_lst[0].replace("'","20")
-    print('Onset: '+temp_lst[0]+' '+temp_lst[1])
-    hr_splt=temp_lst[1].split(':')
-    temp_dt=datetime.strptime(temp_lst[0],'%d.%m.%Y')
-    ttl_sec=(temp_dt-milestone).total_seconds()
-    ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
-    #print(ttl_sec)
-    szr_onset_sec.append(ttl_sec)
-    szr_onset_str.append(temp_lst[0]+' '+temp_lst[1])
+    if not isinstance(htable[2].iloc[a, 1], str):
+        szr_onset_sec.append(np.nan)
+        szr_onset_str.append('NaN')
+    else:
+        temp_lst=htable[2].iloc[a,1].split(' ')
+        temp_lst[0]=temp_lst[0].replace("'","20")
+        print('Onset: '+temp_lst[0]+' '+temp_lst[1])
+        hr_splt=temp_lst[1].split(':')
+        temp_dt=datetime.strptime(temp_lst[0],'%d.%m.%Y')
+        ttl_sec=(temp_dt-milestone).total_seconds()
+        ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
+        #print(ttl_sec)
+        szr_onset_sec.append(ttl_sec)
+        szr_onset_str.append(temp_lst[0]+' '+temp_lst[1])
     
     # Convert offset to seconds since milestone
-    temp_hr=htable[2].iloc[a,2]
-    print('Offset: '+temp_lst[0]+' '+temp_hr)
-    hr_splt=temp_hr.split(':')
-    ttl_sec=(temp_dt-milestone).total_seconds()
-    ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
-    szr_offset_sec.append(ttl_sec)
-    szr_offset_str.append(temp_lst[0]+' '+temp_hr)
+    if not isinstance(htable[2].iloc[a,2],str):
+        szr_offset_sec.append(np.nan)
+        szr_offset_str.append('NaN')
+    else:
+        temp_hr=htable[2].iloc[a,2]
+        print('temp_hr {}'.format(temp_hr))
+        print('Offset: '+temp_lst[0]+' '+temp_hr)
+        hr_splt=temp_hr.split(':')
+        ttl_sec=(temp_dt-milestone).total_seconds()
+        ttl_sec+=int(hr_splt[0])*3600+int(hr_splt[1])*60+float(hr_splt[2])
+        szr_offset_sec.append(ttl_sec)
+        szr_offset_str.append(temp_lst[0]+' '+temp_hr)
 
-    df=szr_offset_sec[a]-szr_onset_sec[a]
-    mnts=np.floor(df/60)
-    scs=df-mnts*60
-    print('Duration: %d minute(s) %f sec' % (mnts,scs))
+    if szr_onset_str=='NaN' or szr_offset_str=='NaN':
+        print('Duration unknown')
+    else:
+        df=szr_offset_sec[a]-szr_onset_sec[a]
+        mnts=np.floor(df/60)
+        scs=df-mnts*60
+        print('Duration: %d minute(s) %f sec' % (mnts,scs))
 
 
 # Create DataFrame
