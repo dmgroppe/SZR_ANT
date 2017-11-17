f6313a1650b18de40dbe48646571443ad212d881
David Groppe <dgroppe@cogsci.ucsd.edu>
2017-11-16 22:56:49 -0500

    train kdsamp added

--- EU_GENERAL/train_smart_srch_multi_patient.py
+++ EU_GENERAL/train_smart_srch_multi_patient.py
@@ -79,12 +79,9 @@ if os.path.exists(model_path)==False:
 # Import list of subjects to use
 #use_subs_df=pd.read_csv(os.path.join(path_dict['szr_ant_root'],'use_subs.txt'),header=None,na_filter=False)
 use_subs_df=pd.read_csv('train_subs.txt',header=None,na_filter=False)
-#test_sub_list=['NA']
-test_sub_list=['1096']
 train_subs_list=[]
 for sub in use_subs_df.iloc[:,0]:
-    if not sub in test_sub_list:
-        train_subs_list.append(sub)
+    train_subs_list.append(sub)
         
 print('Training subs: {}'.format(train_subs_list))
 
--- EU_GENERAL/train_smart_srch_multi_patient_kdsamp.py
+++ EU_GENERAL/train_smart_srch_multi_patient_kdsamp.py
@@ -1,13 +1,14 @@
-# Performs LOOCV on train_subs.txt using a grid search of gamma values and hand set initial C value. You need to
-# set these yourself in the code (it is not in the json parameter file).
-# Some parameters are fed in via a json file like this:
-# {"model_type": "svm",
-# "model_name": "gen_marr_svm_se",
-# "gam": 0.01,
+# Performs LOOCV on test_subs.txt using logreg or rbf SVM. Gamma values are initially random.
+# C starts at 1 and then is adjusted depending on the gap between training and validatin accuracy.
+# Parameters are fed in via a json file like this:
+# {"model_type": "logreg",
+# "model_name": "genLogregSeLog10",
+# "n_rand_params": 1,
 # "patience": 0,
 # "use_ftrs": ["SE"]}
 #
 # TODO: currently code only works with one feature. Need to add others.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -25,10 +26,10 @@ import json
 
 ## Start of main function
 if len(sys.argv)==1:
-    print('Usage: train_smart_srch_multi_patient.py srch_params.json')
+    print('Usage: train_smart_srch_multi_patient_kdsamp.py srch_params.json')
     exit()
 if len(sys.argv)!=2:
-    raise Exception('Error: train_smart_srch_multi_patient.py requires 1 argument: srch_params.json')
+    raise Exception('Error: train_smart_srch_multi_kdsamp.py requires 1 argument: srch_params.json')
 
 # Import Parameters from json file
 param_fname=sys.argv[1]
@@ -53,13 +54,8 @@ print('Weight subjects equally={}'.format(equal_sub_wts))
 # else:
 #     raise Exception('ictal_wind needs to be "small" or "max"')
 use_ftrs=['SE'] #TODO import this from json file
-# n_rand_params=int(params['n_rand_params'])
-# print('# of random initial hyperparameters to try %d' % n_rand_params)
-n_rand_params=1
-gamma_vals=params['gam']
-print('Gamma value: %f' % gamma_vals)
-ini_C=params['C']
-print('Initial C value: %f' % ini_C)
+n_rand_params=int(params['n_rand_params'])
+print('# of random initial hyperparameters to try %d' % n_rand_params)
 patience=int(params['patience'])
 print('# of steps to wait when performance no longer increases %d' % patience)
 
@@ -91,25 +87,51 @@ print('Training subs: {}'.format(train_subs_list))
 
 
 # ftr_root='/Users/davidgroppe/PycharmProjects/SZR_ANT/EU_GENERAL/EU_GENERAL_FTRS/SE/'
-ftr_root=path_dict['eu_gen_ftrs']
+#ftr_root=path_dict['eu_gen_ftrs']
+ftr_root=os.path.join(path_dict['szr_ant_root'],'EU_GENERAL','KDOWNSAMP')
 ftr='SE'
-ftr_info_dict=eu.data_size_and_fnames(train_subs_list, ftr_root, ftr)
+
+n_wind=0
+for sub in train_subs_list:
+    in_fname='kdownsampled_'+str(sub)+'.npz'
+    npz=np.load(os.path.join(ftr_root,in_fname))
+    n_wind+=npz['ftrs_dsamp'].shape[0]
+n_ftrs=npz['ftrs_dsamp'].shape[1]
+print(npz.keys())
+print('Total # of observations %d' % n_wind)
+print('Total # of features %d' % n_ftrs)
+
+# Import and Concatenate downsampled data
+ftrs=np.zeros((n_wind,n_ftrs))
+sub_id=np.zeros(n_wind,dtype=int)
+szr_class=np.zeros(n_wind)
+obs_ct=0
+for sub in train_subs_list:
+    in_fname='kdownsampled_'+str(sub)+'.npz'
+    npz=np.load(os.path.join(ftr_root,in_fname))
+    n_obs_this_sub=npz['ftrs_dsamp'].shape[0]
+    ftrs[obs_ct:obs_ct+n_obs_this_sub,:]=npz['ftrs_dsamp']
+    sub_id[obs_ct:obs_ct+n_obs_this_sub]=sub
+    szr_class[obs_ct:obs_ct+n_obs_this_sub]=npz['szr_class_dsamp']
+    obs_ct+=n_obs_this_sub
+
+# ftr_info_dict=eu.data_size_and_fnames(train_subs_list, ftr_root, ftr)
 #ftrs_tr, targ_labels_tr=eu.import_data(szr_fnames_tr, non_fnames_tr, n_szr_wind_tr, n_non_wind_tr, ftr_dim)
 
-n_dim=ftr_info_dict['ftr_dim']
-n_non_wind=ftr_info_dict['grand_n_non_wind']
-n_szr_wind=ftr_info_dict['grand_n_szr_wind']
-n_wind=n_non_wind+n_szr_wind
-print('Total # of dimensions: %d ' % n_dim)
-print('Total # of szr time windows: %d ' % n_szr_wind)
-print('Total # of non-szr time windows: %d ' % n_non_wind)
-print('Total # of time windows: %d ' % n_wind)
-# print('Total # of files: %d' % f_ct)
+# n_dim=ftr_info_dict['ftr_dim']
+# n_non_wind=ftr_info_dict['grand_n_non_wind']
+# n_szr_wind=ftr_info_dict['grand_n_szr_wind']
+# n_wind=n_non_wind+n_szr_wind
+# print('Total # of dimensions: %d ' % n_dim)
+# print('Total # of szr time windows: %d ' % n_szr_wind)
+# print('Total # of non-szr time windows: %d ' % n_non_wind)
+# print('Total # of time windows: %d ' % n_wind)
+# # print('Total # of files: %d' % f_ct)
 
 # Load training/validation data into a single matrix
-ftrs, szr_class, sub_id=eu.import_data(ftr_info_dict['grand_szr_fnames'], ftr_info_dict['grand_non_fnames'],
-                                       ftr_info_dict['szr_file_subs'],ftr_info_dict['non_file_subs'],
-                                       n_szr_wind, n_non_wind, n_dim)
+# ftrs, szr_class, sub_id=eu.import_data(ftr_info_dict['grand_szr_fnames'], ftr_info_dict['grand_non_fnames'],
+#                                        ftr_info_dict['szr_file_subs'],ftr_info_dict['non_file_subs'],
+#                                        n_szr_wind, n_non_wind, n_dim)
 
 # Set sample weights to weight each subject (and preictal/ictal equally:
 uni_subs=np.unique(sub_id)
@@ -147,9 +169,9 @@ mn_stim_latency = np.zeros((n_train_subs,n_rand_params))
 n_train_steps=np.zeros(n_rand_params)
 #C_vals=np.random.exponential(1,n_rand_params)
 #gamma_vals=np.random.exponential(1,n_rand_params)
-C_vals=np.ones(n_rand_params)*ini_C # Start at ini_C from json file
+C_vals=np.ones(n_rand_params)*10
 # C = SVM regularization parameter, the smaller it is, the stronger the regularization
-#gamma_vals=10**np.random.uniform(-3,0,n_rand_params)
+gamma_vals=10**np.random.uniform(-3,3,n_rand_params)
 #gamma defines how much influence a single training example has. The larger gamma is, the closer other examples must be to be affected.
 best_valid_bal_acc=0
 best_train_bal_acc=0
@@ -167,14 +189,14 @@ tried_valid_acc=list()
 for rand_ct in range(n_rand_params):
     C=C_vals[rand_ct]  # Start with C=1 and then change it according to train-testing error dif
     C_direction=0
-    gam=gamma_vals
-    # print('Random run %d/%d' % (rand_ct+1,n_rand_params))
+    gam=gamma_vals[rand_ct]
+    print('Random run %d/%d' % (rand_ct+1,n_rand_params))
     print('Using gamma value of %.2E' % gam)
 
     best_vbal_acc_this_gam=0 # best balanced accuracy for this value of gamma
     steps_since_best = 0
     for C_loop in range(10): # Note max # of C values to try is 10
-        print('Using C value of %f' % C)
+        print('Using C value of %f (%d/10 tries)' % (C,C_loop))
         temp_models=dict()
 
         # Use LOOCV to estimate accuracy for this value of C and gamma
@@ -204,11 +226,10 @@ for rand_ct in range(n_rand_params):
 
             train_bool = sub_id != left_out_id
             if equal_sub_wts==True:
-                #model.fit(ftrs[train_bool, :], szr_class[train_bool ], sample_weight=samp_wts[subset_id])
                 model.fit(ftrs[train_bool, :], szr_class[train_bool], sample_weight=samp_wts[train_bool])
             else:
                 model.fit(ftrs[train_bool , :], szr_class[train_bool])
-            #model.fit(ftrs[sub_id == 0, :], szr_class[sub_id == 0]) # min training data to test code
+                #model.fit(ftrs[sub_id == 0, :], szr_class[sub_id == 0]) # min training data to test code
 
             # Save model from this left out sub
             temp_models[left_out_ct]=model
@@ -222,10 +243,11 @@ for rand_ct in range(n_rand_params):
                 temp_train_bacc[left_out_ct]+=temp_bacc/(n_train_subs-1)
                 temp_train_sens[left_out_ct]+=temp_sens/(n_train_subs-1)
                 temp_train_spec[left_out_ct]+=temp_spec/(n_train_subs-1)
-
             # temp_train_bacc[left_out_ct], temp_train_sens[left_out_ct ], temp_train_spec[left_out_ct ] = ief.perf_msrs(
             #     szr_class[train_bool],
             #     class_hat[train_bool])
+
+            # Compute performance on validation data
             temp_valid_bacc[left_out_ct], temp_valid_sens[left_out_ct], temp_valid_spec[left_out_ct] = ief.perf_msrs(
                 szr_class[train_bool==False],
                 class_hat[train_bool==False])
@@ -277,10 +299,9 @@ for rand_ct in range(n_rand_params):
         else:
             steps_since_best+=1
 
-        # C_change=np.abs(mn_temp_train_bacc-mn_temp_valid_bacc)*20
-        # if C_change<2:
-        #     C_change=2
-        C_change = 2
+        C_change=np.abs(mn_temp_train_bacc-mn_temp_valid_bacc)*20
+        if C_change<2:
+            C_change=2
         print('C_change=%f' % C_change)
         n_train_steps[rand_ct] += 1
 
@@ -368,15 +389,15 @@ for rand_ct in range(n_rand_params):
          tried_valid_acc=tried_valid_acc,
          best_valid_bal_acc=best_valid_bal_acc,
          best_train_bal_acc=best_train_bal_acc,
+         equal_sub_wts=equal_sub_wts,
          best_C = best_C,
          best_gam=best_gam,
          best_models=best_models,
-         equal_sub_wts=equal_sub_wts,
          ftr_types=ftr_types,
          left_out_id=left_out_id)
 
 print('Done!')
-print('Best training data accuracy: %f' % best_train_bal_acc)
+print('Best training date accuracy: %f' % best_train_bal_acc)
 print('Best validation accuracy: %f' % best_valid_bal_acc)
 print('Using C=%.2E and gam=%.2E' % (best_C,best_gam))
 print('Model name: {}'.format(model_name))
