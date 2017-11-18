4db05e6d66d3c6c40bb5cd431d9ce9076a047f7f
David Groppe <dgroppe@cogsci.ucsd.edu>
2017-11-18 07:01:29 -0500

    C_vals now random

--- EU_GENERAL/train_smart_srch_multi_patient_kdsamp.py
+++ EU_GENERAL/train_smart_srch_multi_patient_kdsamp.py
@@ -173,7 +173,8 @@ mn_stim_latency = np.zeros((n_train_subs,n_rand_params))
 n_train_steps=np.zeros(n_rand_params)
 #C_vals=np.random.exponential(1,n_rand_params)
 #gamma_vals=np.random.exponential(1,n_rand_params)
-C_vals=np.ones(n_rand_params)*10
+#C_vals=np.ones(n_rand_params)*10
+C_vals=10**np.random.uniform(0,3,n_rand_params)
 # C = SVM regularization parameter, the smaller it is, the stronger the regularization
 gamma_vals=10**np.random.uniform(-3,3,n_rand_params)
 #gamma defines how much influence a single training example has. The larger gamma is, the closer other examples must be to be affected.
