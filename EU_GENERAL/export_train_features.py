1ab98d4f77b735024371e5d058434ebc53670300
David Groppe <dgroppe@cogsci.ucsd.edu>
2017-12-28 22:32:54 -0500

    minor

--- EU_GENERAL/export_train_features.py
+++ EU_GENERAL/export_train_features.py
@@ -24,28 +24,29 @@ import json
 
 
 ## Start of main function
-if len(sys.argv)==1:
-    print('Usage: train_smart_srch_multi_patient.py srch_params.json')
-    exit()
-if len(sys.argv)!=2:
-    raise Exception('Error: train_smart_srch_multi_patient.py requires 1 argument: srch_params.json')
+# if len(sys.argv)==1:
+#     print('Usage: train_smart_srch_multi_patient.py srch_params.json')
+#     exit()
+# if len(sys.argv)!=2:
+#     raise Exception('Error: train_smart_srch_multi_patient.py requires 1 argument: srch_params.json')
 
 # Import Parameters from json file
-param_fname=sys.argv[1]
-print('Importing model parameters from %s' % param_fname)
-with open(param_fname) as param_file:
-    params=json.load(param_file)
-model_name=params['model_name']
-print('Model name is %s' % model_name)
-model_type=params['model_type']
-print('Model type is %s' % model_type)
-ftr_types=params['use_ftrs']
-print('Features being used: {}'.format(ftr_types))
-if params['equal_sub_wts']=="False":
-    equal_sub_wts=False
-else:
-    equal_sub_wts = True
-print('Weight subjects equally={}'.format(equal_sub_wts))
+# param_fname=sys.argv[1]
+# print('Importing model parameters from %s' % param_fname)
+# with open(param_fname) as param_file:
+#     params=json.load(param_file)
+# model_name=params['model_name']
+# print('Model name is %s' % model_name)
+# model_type=params['model_type']
+# print('Model type is %s' % model_type)
+# ftr_types=params['use_ftrs']
+# print('Features being used: {}'.format(ftr_types))
+# if params['equal_sub_wts']=="False":
+#     equal_sub_wts=False
+# else:
+#     equal_sub_wts = True
+# print('Weight subjects equally={}'.format(equal_sub_wts))
+
 # if params['ictal_wind']=='small':
 #     small_ictal_wind=True
 # elif params['ictal_wind']=='max':
@@ -55,34 +56,36 @@ print('Weight subjects equally={}'.format(equal_sub_wts))
 use_ftrs=['SE'] #TODO import this from json file
 # n_rand_params=int(params['n_rand_params'])
 # print('# of random initial hyperparameters to try %d' % n_rand_params)
-n_rand_params=1
-gamma_vals=params['gam']
-print('Gamma value: %f' % gamma_vals)
-ini_C=params['C']
-print('Initial C value: %f' % ini_C)
-patience=int(params['patience'])
-print('# of steps to wait when performance no longer increases %d' % patience)
+# n_rand_params=1
+# gamma_vals=params['gam']
+# print('Gamma value: %f' % gamma_vals)
+# ini_C=params['C']
+# print('Initial C value: %f' % ini_C)
+# patience=int(params['patience'])
+# print('# of steps to wait when performance no longer increases %d' % patience)
 
 # Find if there any existing models of this name
 # If so, grab the number of model an increment that number by 1 to get new model name
 path_dict=ief.get_path_dict()
-model_root=model_path=os.path.join(path_dict['szr_ant_root'],'MODELS')
-model_num=1
-for f in os.listdir(model_root):
-    if os.path.isdir(os.path.join(model_root,f)):
-        spltf=f.split('_')
-        if spltf[0]==model_name:
-           temp_model_num=int(spltf[1])
-           if temp_model_num>=model_num:
-               model_num=temp_model_num+1
-model_path=os.path.join(model_root,model_name+'_'+str(model_num))
-print('Model will be stored to %s' % model_path)
-if os.path.exists(model_path)==False:
-    os.mkdir(model_path)
+# model_root=model_path=os.path.join(path_dict['szr_ant_root'],'MODELS')
+# model_num=1
+# for f in os.listdir(model_root):
+#     if os.path.isdir(os.path.join(model_root,f)):
+#         spltf=f.split('_')
+#         if spltf[0]==model_name:
+#            temp_model_num=int(spltf[1])
+#            if temp_model_num>=model_num:
+#                model_num=temp_model_num+1
+# model_path=os.path.join(model_root,model_name+'_'+str(model_num))
+# print('Model will be stored to %s' % model_path)
+# if os.path.exists(model_path)==False:
+#     os.mkdir(model_path)
 
 # Import list of subjects to use
 #use_subs_df=pd.read_csv(os.path.join(path_dict['szr_ant_root'],'use_subs.txt'),header=None,na_filter=False)
-use_subs_df=pd.read_csv('train_subs.txt',header=None,na_filter=False)
+train_sub_fname='train_subsAES.txt'
+print('Importing training subject list: %s' % train_sub_fname)
+use_subs_df=pd.read_csv(train_sub_fname,header=None,na_filter=False)
 #test_sub_list=['NA']
 test_sub_list=['1096']
 train_subs_list=[]
