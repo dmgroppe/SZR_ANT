72639681170b4ac05650e93d9851c2392778a0cb
David Groppe <dgroppe@cogsci.ucsd.edu>
2018-03-16 14:46:28 -0400

    changing to unbounded

--- EU_GENERAL/kmeans_dsamp_per_sub.py
+++ EU_GENERAL/kmeans_dsamp_per_sub.py
@@ -38,7 +38,7 @@ sub=int(sys.argv[1])
 
 # Load raw ftrs
 #ftr_fname='/Users/davidgroppe/Desktop/train_ftrs_aes.npz'
-ftr_fname='train_ftrs_bnded.npz'
+ftr_fname='train_ftrs_unbnded.npz'
 print("loading data from %s" % ftr_fname)
 npz=np.load(ftr_fname)
 print(npz.keys())
